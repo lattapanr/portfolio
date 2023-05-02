79aa4b31b4c0ed609c5bac530eff40feac865b82
Noey <noey.rachata@gmail.com>
2023-05-02 15:57:37 +0700

    Adjust spacing and delete unnecessary code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,81 +4,99 @@ import React from "react";
 import ContactImage from "../assets/contactimage.jpg";
 
 const Contact = () => {
-  return (
-    <div className="w-[75%] h-screen max-w-[1000px] mx-auto flex justify-center items-center md:justify-between gap-8">
-      <div className="max-w-[500px]">
-        <div className="mb-[5rem]">
-          <p className="text-xl">Lattapan</p>
-          <h3 className=" uppercase w-full text-[45px] lg:text-[55px] leading-[3rem]">
-            Let's work together
-          </h3>
+    return (
+        <div className="w-[75%] h-screen max-w-[1000px] mx-auto flex justify-center items-center md:justify-between gap-8">
+            <div className="max-w-[500px]">
+                <div className="mb-8">
+                    <p className="text-xl">Lattapan</p>
+                    <h3 className=" uppercase w-full text-[45px] lg:text-[55px] leading-[3rem]">
+                        Let's work together
+                    </h3>
+                </div>
+
+                <form className="">
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input
+                            type="text"
+                            name="name"
+                            id="contactName"
+                            className="block py-2 px-0 w-full text-base text-primary bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
+                            placeholder=" "
+                            required
+                        />
+                        <label
+                            htmlFor="contactName"
+                            className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Name
+                        </label>
+                    </div>
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="block py-2 px-0 w-full text-base text-primary bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
+                            placeholder=" "
+                            required
+                        />
+                        <label
+                            htmlFor="email"
+                            className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Email
+                        </label>
+                    </div>
+
+                    <div className="relative z-0 w-full mb-6 group">
+                        <input
+                            type="text"
+                            name="subject"
+                            id="subject"
+                            className="block py-2 px-0 w-full text-base text-primary bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
+                            placeholder=" "
+                            required
+                        />
+                        <label
+                            htmlFor="subject"
+                            className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Subject
+                        </label>
+                    </div>
+
+                    <div className="relative z-0 w-full mb-6 group">
+                        <textarea
+                            type="text"
+                            name="message"
+                            id="message"
+                            className="block py-3 px-0 w-full text-base text-primary whitespace-pre-line bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
+                            placeholder=" "
+                            required
+                        />
+                        <label
+                            htmlFor="message"
+                            className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Message
+                        </label>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="text-white bg-primary hover:bg-white hover:text-primary focus:ring-4 focus:outline-none focus:ring-primary font-extrabold rounded-sm text-base w-full sm:w-auto px-5 py-2.5 text-center dark:text-primary dark:font-extrabold dark:bg-white dark:hover:bg-primary dark:hover:text-white dark:focus:ring-white"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+            <img
+                src={ContactImage}
+                alt=""
+                className="hidden md:flex h-[350px] w-[230px] lg:h-[430px] lg:w-[300px] shadow-primary shadow-sm"
+            />
         </div>
-
-        <form className="">
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              type="text"
-              name="name"
-              id="contactName"
-              className="block py-2 px-0 w-full text-base text-primary bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="contactName"
-              className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Name
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="block py-2 px-0 w-full text-base text-primary bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="email"
-              className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Email
-            </label>
-          </div>
-          <div className="relative z-0 w-full mb-6 group">
-            <input
-              type="text"
-              name="message"
-              id="message"
-              className="block py-2 px-0 w-full text-base text-primary bg-transparent border-0 border-b-2 border-primary appearance-none dark:text-white dark:border-white dark:focus:border-white focus:outline-none focus:ring-0 focus:primary peer"
-              placeholder=" "
-              required
-            />
-            <label
-              htmlFor="message"
-              className="peer-focus:font-medium absolute text-lg text-primary dark:text-white duration-300 transform -translate-y-6 scale-75 top-1 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-primary peer-focus:dark:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-            >
-              Message
-            </label>
-          </div>
-
-          <button
-            type="submit"
-            className="text-white bg-primary hover:bg-white hover:text-primary focus:ring-4 focus:outline-none focus:ring-primary font-extrabold rounded-sm text-base w-full sm:w-auto px-5 py-2.5 text-center dark:text-primary dark:font-extrabold dark:bg-white dark:hover:bg-primary dark:hover:text-white dark:focus:ring-white"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
-      <img
-        src={ContactImage}
-        alt=""
-        className="hidden md:flex h-[350px] w-[230px] lg:h-[400px] lg:w-[300px] shadow-primary shadow-sm"
-      />
-    </div>
-  );
+    );
 };
 
 export default Contact;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,80 +5,78 @@ import { BsFillFileEarmarkPersonFill } from "react-icons/bs";
 import { IoMdMail } from "react-icons/io";
 import { FaGithub, FaLinkedin, FaTimes } from "react-icons/fa";
 
-const Footer = ({ darkMode }) => {
-  return (
-    <footer
-      className={`bg-white shadow dark:bg-primary ${darkMode ? "dark" : ""}`}
-    >
-      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-        <div className="flex items-center justify-between">
-          <div className="">
-            <span className="uppercase sm:text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Lattapan
-            </span>
-          </div>
-          <ul className="flex flex-wrap items-center justify-between w-[180px] sm:w-[250px]  cursor-pointer">
-            <li className="group flex relative">
-              <a href="#" target="blank">
-                <FaGithub size={30} />
-              </a>
-              <span
-                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
+const Footer = () => {
+    return (
+        <footer className="bg-white dark:bg-primary">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+                <div className="flex items-center justify-between">
+                    <div>
+                        <span className="uppercase sm:text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            Lattapan
+                        </span>
+                    </div>
+                    <ul className="flex flex-wrap items-center justify-between w-[180px] sm:w-[250px]  cursor-pointer">
+                        <li className="group flex relative">
+                            <a href="#" target="blank">
+                                <FaGithub size={30} />
+                            </a>
+                            <span
+                                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
     -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto"
-              >
-                @lattapanr
-              </span>
-            </li>
-            <li className="group flex relative">
-              <a href="#" target="blank">
-                <FaLinkedin size={30} />
-              </a>
-              <span
-                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
+                            >
+                                @lattapanr
+                            </span>
+                        </li>
+                        <li className="group flex relative">
+                            <a href="#" target="blank">
+                                <FaLinkedin size={30} />
+                            </a>
+                            <span
+                                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
     -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto"
-              >
-                linkedin.com/in/lattapan
-              </span>
-            </li>
-            <li className="group flex relative">
-              <a href="#" target="blank">
-                <IoMdMail size={30} />
-              </a>
-              <span
-                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
+                            >
+                                linkedin.com/in/lattapan
+                            </span>
+                        </li>
+                        <li className="group flex relative">
+                            <a href="#" target="blank">
+                                <IoMdMail size={30} />
+                            </a>
+                            <span
+                                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
     -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto"
-              >
-                lattapanr@gmail.com
-              </span>
-            </li>
-            <li className="group flex relative">
-              <a href="#" target="blank">
-                <BsFillFileEarmarkPersonFill size={30} />
-              </a>
-              <span
-                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
+                            >
+                                lattapanr@gmail.com
+                            </span>
+                        </li>
+                        <li className="group flex relative">
+                            <a href="#" target="blank">
+                                <BsFillFileEarmarkPersonFill size={30} />
+                            </a>
+                            <span
+                                className="group-hover:opacity-100 transition-opacity px-1 text-sm text-primary absolute left-1/2 
     -translate-x-1/2 translate-y-full opacity-0 m-4 mx-auto"
-              >
-                Resume
-              </span>
-            </li>
-          </ul>
-        </div>
-        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
-          <a
-            href="https://github.com/lattapanr"
-            target="blank"
-            className="hover:underline"
-          >
-            Lattapan™
-          </a>
-          . All Rights Reserved.
-        </span>
-      </div>
-    </footer>
-  );
+                            >
+                                Resume
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+                <span className="block text-sm text-gray-500 text-center dark:text-gray-400">
+                    © 2023{" "}
+                    <a
+                        href="https://github.com/lattapanr"
+                        target="blank"
+                        className="hover:underline"
+                    >
+                        Lattapan™
+                    </a>
+                    . All Rights Reserved.
+                </span>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
